17bbd6db644311ebaa86ae77d6e5e1432f08a0dd
Samruddhi Rahegaonkar <rsamruddhi21@gmail.com>
2025-07-26 21:59:30 +0530

    fix: updated logic

--- lib/providers/getitlocator.dart
+++ lib/providers/getitlocator.dart
@@ -1,8 +1,11 @@
-import 'package:badgemagic/providers/imageprovider.dart';
 import 'package:get_it/get_it.dart';
+import 'package:badgemagic/providers/imageprovider.dart';
+import 'package:badgemagic/providers/BadgeAliasProvider.dart';
 
 final GetIt getIt = GetIt.instance;
 
 void setupLocator() {
   getIt.registerLazySingleton<InlineImageProvider>(() => InlineImageProvider());
+
+  getIt.registerLazySingleton<BadgeAliasProvider>(() => BadgeAliasProvider());
 }
