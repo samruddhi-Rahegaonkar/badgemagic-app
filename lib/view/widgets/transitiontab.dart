f339018853091cf48807088be488044b119ad8b5
Samruddhi Rahegaonkar <rsamruddhi21@gmail.com>
2025-08-22 22:24:55 +0530

    fix: added visible scrollbar for Animation tab

--- lib/view/widgets/transitiontab.dart
+++ lib/view/widgets/transitiontab.dart
@@ -10,107 +10,114 @@ class TransitionTab extends StatefulWidget {
 }
 
 class _TransitionTabState extends State<TransitionTab> {
+  final ScrollController _scrollController = ScrollController();
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 4.0),
-            child: Row(
-              children: [
-                AniContainer(
-                  animation: null,
-                  icon: Icons.sports_esports, // Pacman icon
-                  animationName: 'Pacman',
-                  index: 9,
+    return Scrollbar(
+        controller: _scrollController,
+        thumbVisibility: true,
+        thickness: 12.0,
+        radius: const Radius.circular(6),
+        child: SingleChildScrollView(
+          controller: _scrollController,
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 4.0),
+                child: Row(
+                  children: [
+                    AniContainer(
+                      animation: null,
+                      icon: Icons.sports_esports, // Pacman icon
+                      animationName: 'Pacman',
+                      index: 9,
+                    ),
+                    AniContainer(
+                      animation: null,
+                      icon: Icons.chevron_left, // Chevron icon
+                      animationName: 'Chevron',
+                      index: 10,
+                    ),
+                    AniContainer(
+                      animation: null,
+                      icon: Icons.diamond, // Diamond icon
+                      animationName: 'Diamond',
+                      index: 11,
+                    ),
+                  ],
                 ),
-                AniContainer(
-                  animation: null,
-                  icon: Icons.chevron_left, // Chevron icon
-                  animationName: 'Chevron',
-                  index: 10,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 4.0),
+                child: Row(
+                  children: [
+                    AniContainer(
+                      animation: null,
+                      icon: Icons.heart_broken, // Broken Hearts icon
+                      animationName: 'Broken Hearts',
+                      index: 12,
+                    ),
+                    AniContainer(
+                      animation: null,
+                      icon: Icons.favorite_border, // Cupid icon
+                      animationName: 'Cupid',
+                      index: 13,
+                    ),
+                    AniContainer(
+                      animation: null,
+                      icon: Icons.directions_walk, // Feet animation icon
+                      animationName: 'Feet',
+                      index: 14,
+                    ),
+                  ],
                 ),
-                AniContainer(
-                  animation: null,
-                  icon: Icons.diamond, // Diamond icon
-                  animationName: 'Diamond',
-                  index: 11,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 4.0),
+                child: Row(
+                  children: [
+                    AniContainer(
+                      animation: null,
+                      icon: Icons.set_meal, // Fish icon
+                      animationName: 'Fish Kiss',
+                      index: 15,
+                    ),
+                    AniContainer(
+                      animation: null,
+                      icon: Icons.change_history, // V shape icon
+                      animationName: 'Diagonal',
+                      index: 16,
+                    ),
+                    AniContainer(
+                      animation: null,
+                      icon: Icons.warning, // Emergency/alert icon
+                      animationName: 'Emergency',
+                      index: 17,
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 4.0),
+                child: Row(
+                  children: [
+                    AniContainer(
+                      animation: null,
+                      icon: Icons.favorite, // Heart icon
+                      animationName: 'Beating Hearts',
+                      index: 18,
+                    ),
+                    AniContainer(
+                      animation: null,
+                      icon: Icons.celebration, // Fireworks icon
+                      animationName: 'Fireworks',
+                      index: 19,
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 4.0),
-            child: Row(
-              children: [
-                AniContainer(
-                  animation: null,
-                  icon: Icons.heart_broken, // Broken Hearts icon
-                  animationName: 'Broken Hearts',
-                  index: 12,
-                ),
-                AniContainer(
-                  animation: null,
-                  icon: Icons.favorite_border, // Cupid icon
-                  animationName: 'Cupid',
-                  index: 13,
-                ),
-                AniContainer(
-                  animation: null,
-                  icon: Icons.directions_walk, // Feet animation icon
-                  animationName: 'Feet',
-                  index: 14,
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 4.0),
-            child: Row(
-              children: [
-                AniContainer(
-                  animation: null,
-                  icon: Icons.set_meal, // Fish icon
-                  animationName: 'Fish Kiss',
-                  index: 15,
-                ),
-                AniContainer(
-                  animation: null,
-                  icon: Icons.change_history, // V shape icon
-                  animationName: 'Diagonal',
-                  index: 16,
-                ),
-                AniContainer(
-                  animation: null,
-                  icon: Icons.warning, // Emergency/alert icon
-                  animationName: 'Emergency',
-                  index: 17,
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 4.0),
-            child: Row(
-              children: [
-                AniContainer(
-                  animation: null,
-                  icon: Icons.favorite, // Heart icon
-                  animationName: 'Beating Hearts',
-                  index: 18,
-                ),
-                AniContainer(
-                  animation: null,
-                  icon: Icons.celebration, // Fireworks icon
-                  animationName: 'Fireworks',
-                  index: 19,
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
+        ));
   }
 }
