8b6c2513b0bbb376b8116a8bff61e206c42fcde4
Samruddhi Rahegaonkar <rsamruddhi21@gmail.com>
2025-09-03 22:17:47 +0530

    fix: screenshot tests

--- test_integration/screenshots.dart
+++ test_integration/screenshots.dart
@@ -95,7 +95,7 @@ void main() async {
             (widget.child as Padding).child is Image,
       );
 
-      final targetCard = gridViewCard.at(17);
+      final targetCard = gridViewCard.first;
       await tester.tap(targetCard);
       await tester.pumpAndSettle();
       await tester.tap(prefixIcon);
